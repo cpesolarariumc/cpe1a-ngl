2662a82995117bd7c46b63e3eaa46db1302abc48
cpesolarariumc <cpe.solares.johndaril.c@gmail.com>
2026-02-21 17:56:05 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDuMDpKLwAE5FLBS2bzxZb86hl95Ltir8",
+  authDomain: "ngldatabase-5abc1.firebaseapp.com",
+  projectId: "ngldatabase-5abc1",
+  storageBucket: "ngldatabase-5abc1.firebasestorage.app",
+  messagingSenderId: "176091428343",
+  appId: "1:176091428343:web:935f98400d1f2b57c46af2",
+  measurementId: "G-1SYQSDJ74S"
+};
+export const adminPassword = "BSCPE-A1-2ndSem-Admin";
